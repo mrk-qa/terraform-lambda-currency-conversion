31ab2c2a28ecbe145cec4d8808d3f05b7951b912
Marco Antonio <marco.ant.seg@gmail.com>
2024-01-18 12:10:05 -0300

    add iam role and policy lambda

---
+++ iam_lambda.tf
@@ -0,0 +1,38 @@
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com",
+      },
+      Action = ["sts:AssumeRole"],
+    }]
+  })
+}
+
+resource "aws_iam_policy" "lambda_logging" {
+  name        = "lambda_logging"
+  path        = "/"
+  description = "IAM policy for logging from a lambda"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Action = [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      Resource = ["arn:aws:logs:*:*:*"],
+    }],
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_logging.arn
+}
