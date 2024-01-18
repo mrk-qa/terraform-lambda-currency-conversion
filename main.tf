153ff6e73b3992ab1c5ca60d85f6aff8165a8bd9
Marco Antonio <marco.ant.seg@gmail.com>
2024-01-18 12:10:19 -0300

    add main

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = "1.6.6"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.31.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+
+
+  default_tags {
+    tags = {
+      owner      = "marco-qa"
+      managed-by = "terraform"
+    }
+  }
+}
